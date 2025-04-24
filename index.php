edd92caf5eccc4f72cbbf5909039f5f232ca277f
ibrahim <ibrahim.2li@hotmail.com>
2025-04-24 23:16:24 +0300

    section 18

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+// require 'router.php';
+require 'Database.php';
+// connect to our MySQL database.
+
+$db = new Database();
+
+$posts = $db->query("select * from posts where id = 1")->fetch(PDO::FETCH_ASSOC);
+
+
+dd($posts['title']);
